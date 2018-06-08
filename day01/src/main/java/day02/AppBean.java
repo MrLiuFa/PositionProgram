0be4ae3d71c8183a0b3afba0f6b7a7fecdb089c3
MrLiuFa <fajiangxi@163.com>
2018-06-08 09:56:47 +0800

    5555

---
+++ day01/src/main/java/day02/AppBean.java
@@ -0,0 +1,70 @@
+package day02;
+
+public class AppBean {
+	private String date ;
+	private String userName ;
+	private String appName ;
+	private String appStore;
+	private String lowVersion ;
+	private String upVersion ;
+	
+	
+	
+	public AppBean() {
+		super();
+	}
+	
+	public AppBean(String date, String userName, String appName, String appStore, String lowVersion, String upVersion) {
+		super();
+		this.date = date;
+		this.userName = userName;
+		this.appName = appName;
+		this.appStore = appStore;
+		this.lowVersion = lowVersion;
+		this.upVersion = upVersion;
+	}
+
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAppName() {
+		return appName;
+	}
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	public String getAppStore() {
+		return appStore;
+	}
+	public void setAppStore(String appStore) {
+		this.appStore = appStore;
+	}
+	public String getLowVersion() {
+		return lowVersion;
+	}
+	public void setLowVersion(String lowVersion) {
+		this.lowVersion = lowVersion;
+	}
+	public String getUpVersion() {
+		return upVersion;
+	}
+	public void setUpVersion(String upVersion) {
+		this.upVersion = upVersion;
+	}
+	@Override
+	public String toString() {
+		return "AppBean [date=" + date + ", userName=" + userName + ", appName=" + appName + ", appStore=" + appStore
+				+ ", lowVersion=" + lowVersion + ", upVersion=" + upVersion + "]";
+	}
+	
+
+}
